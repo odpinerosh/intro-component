1d37d9a1dff96924a628a038f7e70539ad9437e4
Daniel Piñeros <odpinerosh@gmail.com>
2021-04-27 21:28:12 -0500

    Started JavaScript functions

---
+++ scripts/action.js
@@ -0,0 +1,14 @@
+const txtFirstName = document.getElementById('txtFirstname');
+const txtLastName = document.getElementById('txtLastName');
+const txtMail = document.getElementById('txtMail');
+const txtPassword = document.getElementById('txtPasswoord');
+const bntSubmit = document.getElementById('btnSubmit');
+
+const funValidar = el => {
+    el.preventDefault();
+
+    
+}
+
+
+bntSubmit.addEventListener('click', funValidar);
